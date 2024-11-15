1eae5eaa80a9ea83054c85ad98e70a8cf41f9da7
ultrasuccess <denis.ostry10@gmail.com>
2024-11-15 14:39:43 +0000

    add extra message dev2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Feature branch 1 - development 2");
         }
     }
 }
