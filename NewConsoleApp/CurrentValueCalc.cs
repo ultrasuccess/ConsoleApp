80c58f888070ed483d185a5a6f88f5bcaff31803
ultrasuccess <denis.ostry10@gmail.com>
2024-11-18 16:34:52 +0000

    add assignment 1

---
+++ NewConsoleApp/CurrentValueCalc.cs
@@ -0,0 +1,37 @@
+﻿
+namespace NewConsoleApp
+{
+    public class CurrentValueCalc
+    {
+        private double Nominal { get; set;}
+        private double Price { get; set; }
+        private TransactionType TransactionType { get; set; }
+
+        /// <summary>
+        /// Calculates the current value of a transaction.
+        /// </summary>
+        /// <param name="nominal"></param>
+        /// <param name="price"></param>
+        /// <param name="trcType"></param>
+        /// <returns></returns>
+        public double Calculation(double nominal, double price, TransactionType trcType)
+        {
+            Nominal = nominal;
+            Price = price;
+            TransactionType = trcType;
+
+            double value = nominal * price;
+
+            if (trcType == TransactionType.Buy)
+            {
+                return value;
+            }
+            if (trcType == TransactionType.Sell)
+            {
+                value *= -1;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ NewConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NewConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the nominal of the trade:");
+            string inputNominal = Console.ReadLine();
+            double nominal = double.Parse(inputNominal);
+            if (nominal <= 0)
+            {
+                throw new ArgumentException("Nominal should be positive");
+            }
+
+            Console.WriteLine("Input the price of the trade:");
+            string inputPrice = Console.ReadLine();
+            double price = double.Parse(inputPrice);
+            if (price <= 0)
+            {
+                throw new ArgumentException("Price should be positive");
+            }
+
+            Console.WriteLine("Input the type of the trade:");
+            string inputType = Console.ReadLine();
+            var trcType = (TransactionType) Enum.Parse(typeof(TransactionType), inputType, true);
+
+            // Different approach
+            var currentValue = new CurrentValueCalc();
+            var trcValue = currentValue.Calculation(nominal, price, trcType);
+
+            // Standard approach
+            //var currentValue = nominal * price;
+            //var trcValue = trcType == TransactionType.Buy ? currentValue : (currentValue * -1);
+
+            Console.WriteLine($"Current value of the transaction is {trcValue}");
+
+        }
+    }
+}
